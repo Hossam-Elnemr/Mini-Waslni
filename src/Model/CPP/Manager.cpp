231851876e87b598eee781100c6990018e28bb79
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-04-24 01:07:03 +0200

    Update Manager.cpp

--- src/Model/CPP/Manager.cpp
+++ src/Model/CPP/Manager.cpp
@@ -2,7 +2,24 @@
 # include <string>
 # include <iostream>
 using namespace std;
+
+int Manager::graphsCounter = 0;
+Manager* Manager::instance = nullptr;
+
+Manager::Manager() {
+	
+}
+
 Graph* Manager::getGraph(std::string name) {
 	Graph* g = new Graph(name);
+	graphs.push_back(g);
+	++graphsCounter;
 	return g;
+}
+
+int Manager::totalCities() {
+	int sum = 0;
+	for (auto graph : graphs)
+		sum += (int)graph->cities.size();
+	return sum;
 }
