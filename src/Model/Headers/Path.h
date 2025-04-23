7f8ed074caeb9798e00a18737f35edab867d772e
Remon Ehab <remonehab91@gmail.com>
2025-04-23 17:09:07 +0200

    Update Path.h

--- src/Model/Headers/Path.h
+++ src/Model/Headers/Path.h
@@ -2,5 +2,9 @@
 # include "Edge.h"
 # include <iostream>
 class Path {
-
+public:
+	  vector<string> Path_Edges;
+	  vector<string> Path_Nodes;
+	  double total_lengthCost;
+	  double total_trafficCost;
 };
