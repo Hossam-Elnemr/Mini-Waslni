01bc292ee4c272df83d00d0f67f8d448a36ad893
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-04-24 00:36:10 +0200

    Update Graph.cpp

--- src/Model/CPP/Graph.cpp
+++ src/Model/CPP/Graph.cpp
@@ -5,22 +5,31 @@ Graph::Graph(string name) {
 	this->name = name;
 	id = numberOfGraphs++;
 }
+
+//								Validations
+
+bool Graph::cityIsFound(std::string name) {
+	return cities.find(name) != cities.end();
+}
+bool Graph::EdgeIsFound(std::string name) {
+	return roads.find(name) != roads.end();
+}
+
+
+
+//								  Modify
 void Graph::addCity(string name) {
-	if (cities.find(name) != cities.end()) {
-		std::cout << "City Name is found!\nEnter another name\n";
-		return;
-	}
+	if (cityIsFound(name))
+		return std::cout << "City Name is found!\nEnter another name\n", void();
+
 	Node* newCity = new Node(name);
 	cities[newCity->name] = newCity;
 }
 
-bool Graph::validate() {
-	return true;
-}
-
-
 void Graph::addEdge(string name, string src, string dest, int length, bool directed) {
-	//validate()
+	if (!cityIsFound(src) || !cityIsFound(dest) || EdgeIsFound(name))
+		return std::cout << "Error!!\nEnter a new edge name between two existing cities!\n", void();
+
 
 	Node* source = getNode(src);
 	Node* destination = getNode(dest);
@@ -37,18 +46,15 @@ void Graph::addEdge(string name, string src, string dest, int length, bool direc
 		std::cout << dest << " -> " << src << '\n';;
 	}
 }
-Node* Graph::getNode(string name) {
-	return cities[name];
-}
-Edge* Graph::getEdge(string name) {
-	return roads[name];
-}
 
-int Graph::getID() {
-	return id;
-}
 
+
+//								Traverse
 vector<string> Graph::BFS(string name) {
+	if (!cityIsFound(name))
+		return std::cout << "City not found!\nEnter a valid city.\n", vector<string>();
+
+
 	queue<string> q;
 	unordered_map<string, bool> visited;
 
@@ -56,7 +62,7 @@ vector<string> Graph::BFS(string name) {
 	q.push(name);
 	visited[name] = true;
 	string city;
-	while (!q.empty()) {
+	while(!q.empty()) {
 		string next_city = q.front();
 		q.pop();
 		result.push_back(next_city);
@@ -75,9 +81,35 @@ vector<string> Graph::BFS(string name) {
 	return result;
 }
 
+
+
+
+Node* Graph::getNode(string name) {
+	return cities[name];
+}
+Edge* Graph::getEdge(string name) {
+	return roads[name];
+}
+
+int Graph::getID() {
+	return id;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 void Graph::test() {
 	Graph* g = new Graph("graph1");
-	//Graph* g2 = m.getGraph("graph2");
+	Graph* g2 = new Graph("graph2");
 	g->addCity("Cairo");
 	g->addCity("Giza");
 	g->addCity("Alex");
@@ -88,12 +120,11 @@ void Graph::test() {
 	g->addEdge("fourth edge", "Matrouh", "Cairo", 4, true);
 	g->addEdge("third edge", "Alex", "Aswan", 4, false);
 	//cout << g->roads["First edge"]->graphId;
-
 	vector <string> v;
 	string start = "Matrouh";
 	v = g->BFS(start);
 	std::cout << "\n\nFirst Test:\nSource is " << start << '\n';
-	for (int i = 0; i < v.size(); i++) {
+	for(int i = 0; i < v.size(); i++) {
 		cout << v[i];
 		if (i != v.size() - 1)
 			cout << " -> ";
