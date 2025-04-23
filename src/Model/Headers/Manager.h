0377c74aab291fdf0e48d00d4ff3666833018a96
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-04-24 01:06:33 +0200

    Update Manager.h

--- src/Model/Headers/Manager.h
+++ src/Model/Headers/Manager.h
@@ -1,9 +1,19 @@
-#pragma once
 # include "Graph.h"
 # include <string>
 # include <iostream>
+# include <vector>
 using namespace std;
 class Manager {
+	static Manager* instance;
+	Manager();
 public:
 	Graph* getGraph(std::string name);
+	vector<Graph*> graphs;
+	static int graphsCounter;
+	int totalCities();
+	static Manager* getInstance() {
+		if (instance == nullptr)
+			instance = new Manager();
+		return instance;
+	}
 };
