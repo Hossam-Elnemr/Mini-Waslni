3aa18dd7894cf85bdaacee1fd630d89a7413516a
AnsElwkel <anshesh039@gmail.com>
2025-04-24 10:52:51 +0200

    Update Database Class

--- src/Model/Headers/Database.h
+++ src/Model/Headers/Database.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef DATABASE_H
+#define DATABASE_H
+#include <DynamicList.h>
+/*
+- Main DS will be here
+- If we need to retrieve or modify or insert data generate the getters and setters (make more validation)
+*/
+
+
+class Database {
+	Database() = default;
+	static Database* instance;
+	static DynamicList graph; 
+public:
+	
+	static Database& getInstance();
+
+	Database(const Database&) = delete;
+	Database(const Database&&) = delete;
+	Database& operator=(const Database&) = delete;
+	Database& operator=(const Database&&) = delete;
+	~Database() = default;
+};
+
+
+#endif
