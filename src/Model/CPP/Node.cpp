0326c6b6c4c285ae21b2d59c47098e6691a44c61
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-04-27 22:20:51 +0300

    Update Node.cpp

--- src/Model/CPP/Node.cpp
+++ src/Model/CPP/Node.cpp
@@ -1,13 +1,13 @@
 //# include "Model/Headers/Node.h"
 # include<iostream>
-# include "../Headers/Node.h"
+# include "Node.h"
 using namespace std;
 int Node::numberOfNodes = 0;
 Node::Node(string name) {
 	this->name = name;
 	id = numberOfNodes++;
 }
-string Node::to_string() const noexcept {
+string Node::to_string(){
 	string str = "ID: " + std::to_string(id) + "\nName: " + name + "\nNeighbours: ";
 	for (auto &i : neighbours)
 		str += i + ' ';
