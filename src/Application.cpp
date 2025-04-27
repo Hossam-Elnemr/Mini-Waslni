ae50a9aaa22724f7424449d4e4613e789c08a62d
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-04-27 22:12:42 +0300

    Update Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,15 @@
-# include <iostream>
+/*# include <iostream>
 #include "Model/Headers/MiniWaslni.h"
 int main() {
 	MiniWaslni app;
 	app.run();
 	//cout << "Hello";
+}*/
+
+# include <iostream>
+#include "Manager.h"
+using namespace std;
+int main() {
+	Manager* m = Manager::getInstance();
+	Graph::test();
 }
