0770d9af0b7bf0a17907ee5433d3c4ef14b738ae
zeyad-sabry <beenzeyad@gmail.com>
2025-04-29 16:37:38 +0300

    Update Node.cpp

--- src/Model/CPP/Node.cpp
+++ src/Model/CPP/Node.cpp
@@ -1,18 +1,17 @@
-# include "../Headers/Node.h"
-int Node::numberOfNodes = 0;
+# include<iostream>
+# include "Node.h"
+using namespace std;
 Node::Node(string name) {
 	this->name = name;
-	id = numberOfNodes++;
+	cout << '\n';
+	cout << name << '\n';
 }
-string Node::to_string() {
-	string str = "ID: " + std::to_string(id) + "\nName: " + name + "\nNeighbours: ";
-	for (auto i : neighbours)
-		str += i + ' ';
+string Node::to_string() const noexcept{
+	string str = "Name: " + name + "\nNeighbours: ";
+	for (auto& i : neighbours)
+		str += i.first + ' ';
 	return str;
 }
 Node::~Node() {
 	neighbours.clear();
-}
-void Node::addNeighbour(string name) {
-	neighbours.insert(name);
 }
