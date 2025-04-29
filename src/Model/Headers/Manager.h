62c24144877302bac6bff500b9398151f9d816cb
RehabSobhy9 <144958390+RehabSobhy9@users.noreply.github.com>
2025-04-30 00:22:47 +0300

    Update Manager.h

--- src/Model/Headers/Manager.h
+++ src/Model/Headers/Manager.h
@@ -1,16 +1,30 @@
 # include "Graph.h"
+#include "User.h"
+# include <string>
+# include <iostream>
 using namespace std;
 class Manager {
 	static Manager* instance;
+	 User* curUser;
 	Manager();
 public:
-	Graph* getGraph(std::string name);
 	vector<Graph*> graphs;
+	unordered_map<string,string> users;
 	static int graphsCounter;
-	int totalCities();
+
+
+
 	static Manager* getInstance() {
 		if (instance == nullptr)
 			instance = new Manager();
 		return instance;
 	}
+	int totalCities();
+	bool login(string userName,string password);
+	bool signUp(string userName, string password);
+	void editGraph(User user, int id, int op, string name, string src, string dest, double len, bool directed);
+	void test();
+	void addGraph(string name);
+	Graph* findGraph(int id);
+
 };
