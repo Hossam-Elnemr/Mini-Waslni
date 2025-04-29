cf2ad2237614561b46c2eb65f86de651ab8f32c0
zeyad-sabry <beenzeyad@gmail.com>
2025-04-29 16:36:22 +0300

    Update Edge.h

--- src/Model/Headers/Edge.h
+++ src/Model/Headers/Edge.h
@@ -1,15 +1,19 @@
+#ifndef EDGE_H
+#define EDGE_H
+# include<iostream>
 # include "Node.h"
+# include<string>
 class Edge {
 public:
-	Node* source, * destination;
-	std::string name;
+	std::string name, source, destination;
 	int id, graphId;
 	double length, trafficLoad;
 	static int numberofEdges;
 	bool directed;
-	Edge(Node* src, Node* dest, std::string name, int length, bool directed);
+	Edge(string name, string src, string dest, int length, bool directed);
 	~Edge();
 	void setGraphid(int graphId);
 	double trafficCost();
 	double lengthCost();
 };
+#endif
