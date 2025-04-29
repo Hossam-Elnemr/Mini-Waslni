715c1b52f54273aac47ea82176b4717653a3b07d
zeyad-sabry <beenzeyad@gmail.com>
2025-04-29 16:35:42 +0300

    Update Graph.h

--- src/Model/Headers/Graph.h
+++ src/Model/Headers/Graph.h
@@ -1,4 +1,16 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include<string>
+#include "Node.h"
+#include "Edge.h"
+# include <vector>
+# include <unordered_map>
+# include <map>
 # include "Path.h"
+# include <queue>
+#include <stack>
+
+using namespace std;
 class Graph {
 	Node* getNode(string name);
 	Edge* getEdge(string name);
@@ -6,24 +18,27 @@ class Graph {
 	bool nodeIsFound(std::string);
 	bool edgeIsFound(std::string);
 	int id;
+	bool cur_undo=false;
+	stack <tuple<int, string, vector<Edge>>> lastOperations;
 public:
 	Graph(string name);
 	static int numberOfGraphs;
 	string name;
 	double totalTraffic;
 
-	map<string, Node*> nodes;
-	map<string, Edge*> edges;
+	unordered_map<string, Node*> nodes;
+	unordered_map<string, Edge*> edges;
 
 	//								  Modify graph
 	void addNode(string name);
 	void addEdge(string name, string src, string dest, int length, bool directed);
 	void deleteNode(string name);
 	void deleteEdge(string name);
+	void undo();
 
 	//									Traverse
-	pair<Path, double> shortestPath(string source, string destination);
-	pair<Path, double> fastestPath(string source, string destination);
+	Path shortestPath(string source, string destination);
+	Path fastestPath(string src, string dest);
 	vector<string> DFS(string name);
 	vector<string> BFS(string name);
 
@@ -37,3 +52,4 @@ public:
 		return nodes[name];
 	}
 };
+#endif
