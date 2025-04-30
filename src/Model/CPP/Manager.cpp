0f7599a5eb4409c03cf76645f4a19b068fdfe24b
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-04-30 04:17:17 +0300

    Update Manager.cpp (el fagr)

--- src/Model/CPP/Manager.cpp
+++ src/Model/CPP/Manager.cpp
@@ -1,28 +1,22 @@
 # include "../Headers/Manager.h"
 
-# include <string>
-# include <iostream>
-using namespace std;
-
 int Manager::graphsCounter = 0;
-Manager Manager::instance;
 
 Manager::Manager() {
 	
 }
-
-
 int Manager::totalCities() {
 	int sum = 0;
 	for (auto graph : graphs)
 		sum += (int)graph->nodes.size();
 	return sum;
 }
+
 bool Manager::login(string userName, string password)
 {
 	if (users.find(userName) != users.end() && users[userName] == password)
 		return true;
-	
+
 	return false;
 }
 
@@ -34,26 +28,26 @@ bool Manager::signUp(string userName, string password)
 		users[userName] = password;
 		return true;
 	}
-	
+
 	return false;
 }
 
-void Manager::editGraph(User user, int id,int op,string name,string src,string dest,double len,bool directed)
+void Manager::editGraph(User user, int id, int op, string name, string src, string dest, double len, bool directed)
 {
 	if (user.editGraphCheck(id))
 	{
 		Graph* found = NULL;
-		
-			found = findGraph(id);
-		if (op == 1)//Add Node
+
+		found = findGraph(id);
+		if (op == 1) //Add Node
 		{
 			found->addNode(name);
 		}
-		else if (op == 2)// Add Edge
+		else if (op == 2) // Add Edge
 		{
 			found->addEdge(name, src, dest, len, directed);
 		}
-		else if (op == 3)//Delete Node
+		else if (op == 3) //Delete Node
 		{
 			found->deleteNode(name);
 		}
@@ -63,12 +57,11 @@ void Manager::editGraph(User user, int id,int op,string name,string src,string d
 		}
 
 	}
-	
+
 }
 
 
-Graph* Manager::findGraph(int id)
-{
+Graph* Manager::findGraph(int id) {
 	for (auto graph : graphs)
 		if (graph->getID() == id)
 			return graph;
@@ -80,7 +73,7 @@ void Manager::test()
 	int choice;
 	do {
 		cout << " Login Press 1\nSign up press 2 \nExis press 0\n";
-		
+
 		cin >> choice;
 		string username, pass;
 		cout << "Enter Username:   ";
@@ -109,4 +102,3 @@ void Manager::addGraph(string name)
 	graphs.push_back(newGraph);
 	++graphsCounter;
 }
-
