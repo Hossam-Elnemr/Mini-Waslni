3ecc08f6b37e9c4c7c9ac265832e11dfdea2082c
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-17 21:54:37 +0300

    Update Tools.cpp

--- src/Helpers/Tools.cpp
+++ src/Helpers/Tools.cpp
@@ -4,3 +4,27 @@ bool Tools::isSpecialChar(char c) {
 		|| c == '!' || c == '&' || c == '%' || c == '(' || c == ')' || c == '~' || c == '$' || c == '?'
 		|| c == '{' || c == '}' || c == '[' || c == ']' || c == ':' || c == '|' || c == '+' || c == '-';
 }
+
+bool Tools::validateName(string name, QMainWindow* w) {
+    if(name.empty() || !((name[0]<='z' && name[0]>='a') || (name[0]<='Z' && name[0]>='A'))) {
+        FirstCharacter(w);
+        return false;
+    }
+    return true;
+}
+
+void Tools::FirstCharacter(QMainWindow* w) {
+    QMessageBox::warning(w, "Invalid Input", "First character MUST be alphabet.");
+}
+
+bool Tools::isEqual(string s1, string s2) {
+    if(s1.size() != s2.size())
+        return false;
+    int sz = s1.size();
+    for(int i = 0; i<sz; ++i) {
+        s1[i] = tolower(s1[i]);
+        s2[i] = tolower(s2[i]);
+    }
+    cout << "Testing equal: " << s1 << ' ' << s2 << '\n';
+    return s1 == s2;
+}
