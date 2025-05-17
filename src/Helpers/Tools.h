1c11c01cb8cd15807e0b4a256d199af9d1892b93
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-17 21:55:00 +0300

    Update Tools.h

--- src/Helpers/Tools.h
+++ src/Helpers/Tools.h
@@ -1,5 +1,11 @@
-# include "../Model/Headers/Globals.h"
+#pragma once
+# include "Globals.h"
+# include "QMessageBox.h"
 class Tools {
 public:
 	static bool isSpecialChar(char c);
+    static bool validateName(string name, QMainWindow*); // validations on edge/node/graph/user name
+    void static FirstCharacter(QMainWindow*);
+    void static bridgeFound(QMainWindow*);
+    bool static isEqual(string s1, string s2);
 };
