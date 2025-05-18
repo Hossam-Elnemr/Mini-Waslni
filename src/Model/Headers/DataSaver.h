8e921debd56509d8832ab9ac7523dedc57b2ee6c
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-18 17:15:32 +0300

    Update DataSaver.h

--- src/Model/Headers/DataSaver.h
+++ src/Model/Headers/DataSaver.h
@@ -1,13 +1,34 @@
+#pragma once
 #ifndef DataSaver_H
 #define DataSaver_H
 #include "Globals.h"
-#include "../Headers/Manager.h"
-#include "../Headers/FileManager.h"
+#include "Manager.h"
+#include "FileManager.h"
 namespace Model {
 	class DataSaver {
 	public:
-		static void saveGraphData();
-		static void saveUserData();
+        static void saveGraphData() {
+            vector<string> fileContent;
+            fileContent.push_back(std::to_string(Manager::getInstance()->graphsCounter));
+
+            for (auto graph : Manager::getInstance()->graphs)
+                fileContent.push_back(graph->toString());
+
+            FileManager::saveToFile(fileContent, "graphData.txt");
+        }
+        static void saveUserData() {
+            vector<string> fileContent;
+            fileContent.push_back(std::to_string(Manager::getInstance()->users.size()));
+
+            for (const pair<string,User*>&  user : Manager::getInstance()->users)
+                fileContent.push_back(user.second->toString());
+
+            FileManager::saveToFile(fileContent, "userData.txt");
+        }
+        static void save() {
+            saveGraphData();
+            saveUserData();
+        }
 	};
 }
 #endif
