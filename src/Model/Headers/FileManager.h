88645334331816a6dedd70a7aa6cec1568020aba
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-18 17:14:00 +0300

    Update FileManager.h

--- src/Model/Headers/FileManager.h
+++ src/Model/Headers/FileManager.h
@@ -1,11 +1,38 @@
+#pragma once
 #ifndef FileManager_H
 #define FileManager_H
-#include "../Headers/Globals.h"
+#include "Globals.h"
 namespace Model {
 	class FileManager {
 	public:
-		static void saveToFile(const vector<string>& fileContent, const std::string& filePath);
-		static vector<string> readFromFile(const std::string& filePath);
+        static void saveToFile(const vector<string>& fileContent, const std::string& filePath) {
+            ofstream fout(filePath, ios::out | ios::trunc);
+            if (fout.is_open()) {
+                for (const auto& line : fileContent) {
+                    fout << line << endl;
+                }
+                fout.close();
+            }
+            else {
+                cout << "Unable to open file for writing: " << filePath << endl;
+            }
+        }
+        static vector<string> readFromFile(const std::string& filePath){
+            ifstream file(filePath);
+            vector<string> lines;
+            if (file.is_open()) {
+                string line;
+                while (getline(file, line)) {
+                    lines.push_back(line);
+                }
+                file.close();
+            }
+            else {
+                cout << "Unable to open file for reading: " << filePath << endl;
+            }
+            return lines;
+        }
+
 	};
 }
 #endif
