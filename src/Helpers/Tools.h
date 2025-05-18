b1fff7a2cf9bc3528ac4aa73ab70ddc1fa73dcbf
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-18 20:37:07 +0300

    Update Tools.h

--- src/Helpers/Tools.h
+++ src/Helpers/Tools.h
@@ -5,6 +5,7 @@ class Tools {
 public:
 	static bool isSpecialChar(char c);
     static bool validateName(string name, QMainWindow*); // validations on edge/node/graph/user name
+    static bool validateSize(string name, QMainWindow*);
     void static FirstCharacter(QMainWindow*);
     void static bridgeFound(QMainWindow*);
     bool static isEqual(string s1, string s2);
