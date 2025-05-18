11799e6cf2caf28b5d96d7e38f9e329a62e3ab0a
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-18 16:52:22 +0300

    time and length of dijkstra

--- src/Model/CPP/Edge.cpp
+++ src/Model/CPP/Edge.cpp
@@ -1,8 +1,14 @@
-# include "../Headers/Edge.h"
+# include "Edge.h"
 int Edge::numberofEdges = 0;
-Edge::Edge(std::string name, std::string src, std::string dest, int length, bool directed, int id, int graphId, double trafficLoad) : source(src), destination(dest),
-name(name), length(length), directed(directed) , id(id), graphId(graphId) , trafficLoad(trafficLoad){
+double Edge::constant = 60.0;
+Edge::Edge(std::string name, std::string src, std::string dest, int length, bool directed) : source(src), destination(dest),
+name(name), length(length), trafficLoad(0), directed(directed) {
 	id = numberofEdges++;
+    trafficLength = length;
+}
+Edge::Edge(std::string name, std::string src, std::string dest, int length, bool directed, int id, int graphId, double trafficLoad) : source(src), destination(dest),
+    name(name), length(length), directed(directed) , id(id), graphId(graphId) , trafficLoad(trafficLoad){
+    id = numberofEdges++;
 }
 void Edge::setGraphid(int graphid) {
 	graphId = graphid;
@@ -11,11 +17,15 @@ double Edge::lengthCost() {
 	return length;
 }
 string Edge::to_string() const {
-	return  name + "," + source + "," + destination + "," + std::to_string(length) + "," + std::to_string(directed) + "," + std::to_string(id) + "," + std::to_string(graphId) + "," + std::to_string(trafficLoad) +",";
+    return  name + "," + source + "," + destination + "," + std::to_string(length) + "," + std::to_string(directed) + "," + std::to_string(id) + "," + std::to_string(graphId) + "," + std::to_string(trafficLoad) +",";
 }
 double Edge::trafficCost(double sum) {
-	return length*(1 + trafficLoad/sum);
+    return length + trafficLoad;
 }
+
 Edge::~Edge() {
-	//	DO NOT DELETE SOURCE & DESTINATION
+    // DO NOT DELETE SOURCE & DESTINATION
+}
+double Edge::getTime(double d) {
+    return d/constant;
 }
