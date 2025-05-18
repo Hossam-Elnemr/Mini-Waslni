ee2d65aa84b8124e9dfd204cb428301fa40031ce
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-18 17:14:39 +0300

    Update Edge.h

--- src/Model/Headers/Edge.h
+++ src/Model/Headers/Edge.h
@@ -1,18 +1,19 @@
-#ifndef Edge_H
-#define Edge_H
+#pragma once
 # include "Node.h"
 class Edge {
 public:
 	string name, source, destination;;
+    static double constant;
 	int id, graphId;
-	double length, trafficLoad;
+    double length, trafficLoad, trafficLength;
 	static int numberofEdges;
 	bool directed;
-	Edge(string name, string src, string dest, int length, bool directed, int id, int graphId, double trafficLoad);
+	Edge(string name, string src, string dest, int length, bool directed);
+    Edge(string name, string src, string dest, int length, bool directed, int id, int graphId, double trafficLoad);
 	~Edge();
 	void setGraphid(int graphId);
 	double trafficCost(double sum);
-	double lengthCost();
-	string to_string() const;
+    double lengthCost();
+    double getTime(double d);
+    string to_string() const;
 };
-#endif
