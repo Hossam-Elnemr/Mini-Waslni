81a81f5db0b5151443cfc1b6c32333afd39a44f8
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-18 20:34:39 +0300

    Update DataLoader.h

--- src/Model/Headers/DataLoader.h
+++ src/Model/Headers/DataLoader.h
@@ -1,14 +1,29 @@
+#pragma once
 #ifndef DataLoader_H
 #define DataLoader_H
 #include "Globals.h"
-#include "../Headers/Manager.h"
-#include "../Headers/FileManager.h"
+#include "Manager.h"
+#include "FileManager.h"
 
 namespace Model {
 	class DataLoader {
 	public: 
         static void loadGraphsFromFile(const vector<string>& fileContent) ;
 		static void loadUsersFromFile(const vector<string>& fileContent);
+        static void loadGraphs() {
+            vector<string> fileContent = Model::FileManager::readFromFile("graphData.txt");
+            // cout << "File content size: " << fileContent.size() << "\n";
+            Model::DataLoader::loadGraphsFromFile(fileContent);
+        }
+        static void loadUsers() {
+            vector<string> fileContent = Model::FileManager::readFromFile("userData.txt");
+            Model::DataLoader::loadUsersFromFile(fileContent);
+            cout << "Finished loadUserTest\n";
+        }
+        static void load() {
+            loadGraphs();
+            loadUsers();
+        }
 	};
 }
 #endif
