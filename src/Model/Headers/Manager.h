2021a38e19dd40124aae49d498ac32dfc5cbf42d
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-18 17:11:05 +0300

    Update Manager.h

--- src/Model/Headers/Manager.h
+++ src/Model/Headers/Manager.h
@@ -1,32 +1,41 @@
-#ifndef Manager_H
-#define Manager_H
+#pragma once
 # include "Graph.h"
 # include "User.h"
-// # include "../CPP/DataSaver.cpp"
 using namespace std;
 class Manager {
-	static Manager instance;
-	User* curUser;
+    static Manager* instance;
+
 	Manager();
 public:
+    static User* curUser;
+    static Graph* curGraph;
 	vector<Graph*> graphs;
-	unordered_map<string, User*> users; // username -> User
+    unordered_map<string, User*> users;
+    unordered_map<string, string> usersInfo; // username -> password
 	static int graphsCounter;
+    static int usersCounter;
 
 	//						User Functions
+    User* addUser(string userName, string password);
 	bool login(string userName, string password);
 	bool signUp(string userName, string password);
 
 	//						Graph Functions
-	int totalCities();
-	void editGraph(User user, int id, int op, string name, string src, string dest, double len, bool directed);
-	void addGraph(string name);
-	Graph* findGraph(int id);
+    Graph* addGraph(string name);
+    void editGraph(User user, int id, int op, string name, string src, string dest, double len, bool directed);
+    void enterGraph(Graph* graph);
+    int totalCities();
+    void addFromFile(int graphId, string graphName, vector<vector<string>> nodes, vector<vector<string>> edges);
+    Graph* findGraph(int id, string name);
+    User* findUser(string username);
 
 	//						Getters && Testing
 	void test();
-	static Manager& getInstance() {
+    static void testGraph();
+    static Manager* getInstance() {
+        if(instance == NULL)
+            instance = new Manager();
 		return instance;
 	}
+    void start();
 };
-#endif
