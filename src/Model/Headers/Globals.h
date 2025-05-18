754bd2611100fa16469ff2991e37d7b9dab68082
Hossam ElNemr <hossam.a.elnemr@gmail.com>
2025-05-18 17:09:34 +0300

    Update Globals.h

--- src/Model/Headers/Globals.h
+++ src/Model/Headers/Globals.h
@@ -16,4 +16,40 @@
 #include <limits>
 #include <cmath>
 #include <stdexcept>
+#include<QRandomGenerator>
+#include<QGraphicsTextItem>
+#include<qtimer.h>
+#include<qmessagebox.h>
+#include <QToolTip>
+#include <QPropertyAnimation>
+#include <Qobject>
+#include "ui_GraphPage.h"
+#include <QGraphicsView>
+#include <QDebug>
+#include  <Qstring>
+#include <QGroupBox>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <Qstring>
+#include <QGraphicsTextItem>
+#include <QPainter>
+#include <QPen>
+#include<QtCore>
+#include <QGraphicsLineItem>
+#include <QMouseEvent>
+#include <QPoint>
+#include <QPainterPath>
+#include <QPolygon>
+#include <QVector>
+#include <QWidget>
+#include <QToolTip>
+#include <QPropertyAnimation>
+#include <QGraphicsPixmapItem>
+#include <QDebug>
+#include<QGraphicsDropShadowEffect>
+#include <QDir>
+#include <QTextStream>
+#include <QFile>
+#include <QInputDialog>
+#include <QMessageBox>
 using namespace std;
